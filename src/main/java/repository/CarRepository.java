765b5710ce18080c559d132d588638aacd0a9b6d
BinMa <binma@thoughtworks.com>
2017-10-06 14:36:35 +0800

    add jpa support

--- src/main/java/repository/CarRepository.java
+++ src/main/java/repository/CarRepository.java
@@ -1,6 +1,7 @@
 package repository;
 
 import entity.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,7 +9,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.persistence.EntityManager;
+
 public class CarRepository {
+
+    private EntityManager entityManager;
+
+    public CarRepository(@Autowired EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     public Car getById(String id) {
         Connection con = null;
@@ -34,5 +43,9 @@ public class CarRepository {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public Car findById(String id) {
+        return entityManager.find(Car.class, id);
     }
 }
