ed865730a0c2f3e3d3a9028ccb7833168f1f4d97
BinMa <binma@thoughtworks.com>
2017-10-06 14:20:37 +0800

    try with resource

--- src/main/java/repository/CarRepository.java
+++ src/main/java/repository/CarRepository.java
@@ -13,32 +13,25 @@ public class CarRepository {
     public Car getById(String id) {
         Connection con = null;
         PreparedStatement stmt = null;
-        ResultSet resultSet = null;
+
 
         try {
             String sql = "SELECT * FROM CAR WHERE ID = ?";
             con = DriverManager.getConnection("localhost:3306/cars");
             stmt = con.prepareStatement(sql);
             stmt.setString(1, id);
-            resultSet = stmt.executeQuery();
-
-            if (resultSet.next()) {
-                Car car = new Car();
-                car.setBrand(resultSet.getString(1));
-                car.setName(resultSet.getString(2));
-                return car;
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (resultSet != null && !resultSet.isClosed()) {
-                    resultSet.close();
+            try (ResultSet resultSet = stmt.executeQuery()) {
+                if (resultSet.next()) {
+                    Car car = new Car();
+                    car.setBrand(resultSet.getString(1));
+                    car.setName(resultSet.getString(2));
+                    return car;
+                } else {
+                    return null;
                 }
-            } catch (SQLException e) {
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return null;
     }
