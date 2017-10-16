7d0b302e4a8555d6a6517188e11e75998899cf08
BinMa <binma@thoughtworks.com>
2017-10-16 17:25:43 +0800

    autowire using constructor

--- src/main/java/com/thoughtworks/service/CustomerServiceImpl.java
+++ src/main/java/com/thoughtworks/service/CustomerServiceImpl.java
@@ -3,16 +3,16 @@ package com.thoughtworks.service;
 import com.thoughtworks.entity.Customer;
 import com.thoughtworks.repository.CustomerRepository;
 
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class CustomerServiceImpl implements CustomerService {
 
-    @Setter(onMethod = @__(@Autowired))
     private CustomerRepository customerRepository;
 
     @Override
