decf0cb946ff058177ea1a8b4e86c31325d96ad3
BinMa <binma@thoughtworks.com>
2017-10-16 17:23:14 +0800

    autowire using setter

--- src/main/java/com/thoughtworks/service/CustomerServiceImpl.java
+++ src/main/java/com/thoughtworks/service/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.thoughtworks.service;
 import com.thoughtworks.entity.Customer;
 import com.thoughtworks.repository.CustomerRepository;
 
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    @Autowired
+    @Setter(onMethod = @__(@Autowired))
     private CustomerRepository customerRepository;
 
     @Override
