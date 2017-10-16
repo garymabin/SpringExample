ff2361f45f5c68ba30ba8fa1e0b86941221d5b83
BinMa <binma@thoughtworks.com>
2017-10-16 18:18:15 +0800

    @Autowired in java based configuration

--- src/main/java/com/thoughtworks/AppConfig.java
+++ src/main/java/com/thoughtworks/AppConfig.java
@@ -6,16 +6,16 @@ import com.thoughtworks.service.CustomerService;
 import com.thoughtworks.service.CustomerServiceImpl;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.thoughtworks")
 public class AppConfig {
 
     @Bean(name = "customerService")
     public CustomerService getCustomerService() {
         CustomerServiceImpl customerService =  new CustomerServiceImpl();
-        customerService.setCustomerRepository(getCustomerRepository());
-
         return customerService;
     }
 
--- src/main/java/com/thoughtworks/service/CustomerServiceImpl.java
+++ src/main/java/com/thoughtworks/service/CustomerServiceImpl.java
@@ -3,13 +3,13 @@ package com.thoughtworks.service;
 import com.thoughtworks.entity.Customer;
 import com.thoughtworks.repository.CustomerRepository;
 
-import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    @Setter
+    @Autowired
     private CustomerRepository customerRepository;
 
     @Override
