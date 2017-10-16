48ab87f4641ff2a27f21e437af30d3f84fb2955e
BinMa <binma@thoughtworks.com>
2017-10-16 16:24:48 +0800

    add bean constructor injection

--- src/main/java/com/thoughtworks/service/CustomerServiceImpl.java
+++ src/main/java/com/thoughtworks/service/CustomerServiceImpl.java
@@ -3,11 +3,13 @@ package com.thoughtworks.service;
 import com.thoughtworks.entity.Customer;
 import com.thoughtworks.repository.CustomerRepository;
 
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Setter
+@AllArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
     private CustomerRepository customerRepository;
 
